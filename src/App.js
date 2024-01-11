4827e5aa9418951b9de5cdaa1fab1d8fe11f34ce
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-11 22:20:22 +0300

    store added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from "react-redux";
 import AppRouter from "./router/AppRouter";
+import store from "./app/store";
 
 function App() {
   return (
     <div>
-      <AppRouter />
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </div>
   );
 }
