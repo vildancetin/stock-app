c586fe3784d842c5df6d54eef1c3f45a04a16dbc
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-13 23:45:31 +0300

    redux-persist added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Provider } from "react-redux";
 import AppRouter from "./router/AppRouter";
-import store from "./app/store";
-
+import store,{persistor} from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <AppRouter />
+        <PersistGate loading={null} persistor={persistor}>
+          <AppRouter />
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import authReducer from "../features/authSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
-
+export const persistor = persistStore(store);
 export default store;
