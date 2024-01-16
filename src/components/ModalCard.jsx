14a4df633dd8687781ba87a7ef39a85f375d02f7
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-16 23:23:56 +0300

    modal added

---
+++ src/components/ModalCard.jsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+
+import { useState } from "react";
+
+const ModalCard = ({ openModal, handleClose }) => {
+  const [info, setInfo] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    image: "",
+  });
+  const handleChange = (e) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+  console.log(info);
+  return (
+    <>
+      <Modal dismissible show={openModal} onClose={handleClose}>
+        <Modal.Body>
+          <div className="space-y-3">
+            <div>
+              <TextInput
+                id="name"
+                placeholder="name *"
+                required
+                name="name"
+                type="text"
+                value={info.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <TextInput
+                id="phone"
+                type="text"
+                required
+                placeholder="phone *"
+                name="phone"
+                value={info.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <TextInput
+                id="address"
+                type="text"
+                required
+                placeholder="address *"
+                name="address"
+                value={info.address}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <TextInput
+                id="image"
+                type="url"
+                required
+                placeholder="image *"
+                name="image"
+                value={info.image}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={handleClose}
+            type="submit"
+            className="bg-green hover:bg-[#FEBB22]"
+          >
+            Add Firm
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+export default ModalCard;
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,9 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useStockCalls from "../service/useStockCalls";
 import FirmCard from "../components/FirmCard";
 import { useSelector } from "react-redux";
+import ModalCard from "../components/ModalCard";
 
 const Firms = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const handleOpen=()=>{setOpenModal(true)}
+  const handleClose=()=>{setOpenModal(false)}
   const {firms}=useSelector(state=>state.stock)
   const { getFirms } = useStockCalls();
   useEffect(() => {
@@ -11,11 +15,12 @@ const Firms = () => {
   }, []);
   return (
     <div>
-      <button className="inline-block rounded border border-[#537D3C] bg-[#FEBB22] px-5 py-3 mt-5 ml-7 text-sm font-medium text-white  hover:bg-[#537d3c] focus:ring-2 focus:ring-[#537d3c] active:green uppercase">
+      <button className="inline-block rounded border border-[#537D3C] bg-[#FEBB22] px-5 py-3 mt-5 ml-7 text-sm font-medium text-white  hover:bg-[#537d3c] focus:ring-2 focus:ring-[#537d3c] active:green uppercase" onClick={handleOpen}>
         Add Firm
       </button>
+      <ModalCard openModal={openModal} handleClose={handleClose}/>
       <div className="grid sm:grid-col-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-4 mx-7 mt-4 justify-center">
-      {firms.map(firm=><FirmCard firm={firm}/>)}
+      {firms.map(firm=><FirmCard firm={firm} key={firm._id}/>)}
 
       </div>
 
