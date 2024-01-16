b6c16248d4d8208a320a99e8b153f982b7bdf5d7
Vildan Beyza Çetin <vcetin1356@gmail.com>
2024-01-16 10:44:48 +0300

    axios inheritance created

---
+++ src/service/useAxios.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const useAxios = () => {
+  const { token } = useSelector((state) => state.auth);
+
+  const axiosWithToken = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    headers: { Authorization: `Token ${token}` },
+  });
+  const axiosPublic = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  });
+  return { axiosPublic, axiosWithToken };
+};
+
+export default useAxios;
